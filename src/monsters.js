ea2fa1fe6632f1858eabf9e7024252040119a138
rupali.sheth <rupali.sheth@crossasyst.com>
2022-02-08 16:16:07 +0530

    first commit

---
+++ src/monsters.js
@@ -0,0 +1,36 @@
+//App.js code
+
+/*
+import React, { Component } from 'react';
+//import { ReactDOM } from 'react';
+
+class App extends Component {
+
+  constructor() {
+    super();
+    this.handleClick2 = this.handleClick1.bind(this);
+    
+  }
+  
+  handleClick1(){
+    console.log("Button 1 is Clicked");
+  }
+
+  handleClick3 = () =>
+  {
+    console.log("Button 3 is clicked");
+  }
+  render() {
+    return (
+    <div>
+      <button onClick={this.handleClick1()}>Click 1</button>
+      <button onClick={this.handleClick1()}>Click 2</button>
+      <button onClick={this.handleClick2()}>Click 3</button>
+      <button onClick={this.handleClick3()}>Click 4</button>
+    </div>
+    );
+  }
+}
+export default App;
+
+*/
